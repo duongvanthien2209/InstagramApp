2686770365316444d1952d62fa33f71451e68405
Duong Van Thien <duongthien2209@gmail.com>
2020-11-23 10:01:43 +0700

    add eslint-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended', 'airbnb', 'react-app', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -11,8 +11,24 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        tabWidth: 2,
+        semi: true,
+        bracketSpacing: true,
+        printWidth: 80,
+        jsxBracketSameLine: false,
+        trailingComma: 'all',
+        endOfLine: 'lf',
+        arrowParens: 'always',
+        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+        'no-unused-vars': ['error'],
+      },
+    ],
   },
 };
